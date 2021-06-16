898ad978b08bb85a6979cbcb18e4834b469b2a19
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-06-16 22:52:26 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,7 @@
-apt update && apt upgrade
-apt install proot -y
-apt install wget -y
-apt install git
+pkg update && pkg upgrade
+pkg install proot -y
+pkg install wget -y
+pkg install git -y
 git clone https://github.com/MFDGaming/ubuntu-in-termux
 ls
 cd ubuntu-in-termux
